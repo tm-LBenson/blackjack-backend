7b99a5839b0228a0fe023f2d8be65d28b2ca449a
Lewis Benson <tm.lewis.benson@gmail.com>
2023-09-28 16:50:51 -0400

    add game logic to socket server

--- src/socket.js
+++ src/socket.js
@@ -1,21 +1,40 @@
 'use strict';
 
-const { currentNames, refreshName } = require('./userNameCache');
+const GameState = require('./blackjack/GameState');
+const ListOfGames = require('./blackjack/ListOfGames');
+const currentNames = require('./userNameCache');
 
-let io;
+let connectedSockets = [];
 const startIo = (incomingIo) => {
-  io = incomingIo || {};
+  const io = incomingIo || {};
 
   io.on('connection', (socket) => {
     console.log('IO server connection');
+    connectedSockets.push(socket);
+    if (currentNames.names) {
+      socket.emit('USERS', [
+        Object.keys(currentNames.names),
+        ListOfGames.getAllGames(),
+      ]);
+    }
+
+    socket.on('USERS', () => {
+      console.log('sending user and game list');
+      if (currentNames.names) {
+        socket.emit('USERS', [
+          Object.keys(currentNames.names),
+          ListOfGames.getAllGames(),
+        ]);
+      }
+    });
 
     socket.on('CHECK', (username) => {
       let matchFound = false;
 
-      for (const listUsername in currentNames) {
+      for (const listUsername in currentNames.names) {
         if (listUsername === username) {
           matchFound = true;
-          console.log(matchFound);
+
           break;
         }
       }
@@ -23,35 +42,94 @@ const startIo = (incomingIo) => {
     });
 
     socket.on('HEARTBEAT', (username) => {
-      refreshName(username);
+      currentNames.refreshName(username);
     });
 
-    socket.on('USERS', () => {
-      console.log('sending user list');
-      socket.emit('USERS', Object.keys(currentNames));
-     
+    socket.on('CREATE', (payload) => {
+      const [gameId, username] = payload;
+      const gameState = new GameState();
+
+      gameState.addPlayer(username);
+
+      ListOfGames.addGame(gameId, gameState);
+      socket.emit('USERS', [
+        Object.keys(currentNames.names),
+        ListOfGames.getAllGames(),
+      ]);
+      console.log(`Joined room: ${gameId}`);
+      socket.to(gameId).emit('USER_CONNECTED', 'New user connected.');
+      socket.emit('CREATE', gameId);
+
+      io.emit('NEW_ROOM', ListOfGames.getAllGames());
     });
 
-    socket.on('JOIN', (userId) => {
-      socket.join(userId);
-      console.log(`Joined room: ${userId}`);
-      socket.to(userId).emit('USER_CONNECTED', 'New user connected.');
-      socket.emit('JOIN', userId);
+    socket.on('JOIN_GAME', (clientData) => {
+      let { username, roomId } = clientData;
+      if (!roomId) roomId = 1;
+      ListOfGames.games[roomId].addPlayer(username);
+      socket.join(+roomId);
+      socket.to(+roomId).emit('JOIN_GAME', ListOfGames.games[roomId]);
     });
 
-    socket.on('GET_ROOMS', () => {
-      let availableRooms = [];
-      let rooms = io.sockets.adapter.rooms;
+    socket.on('PLAY_GAME', (gameId) => {
+      socket.join(+gameId);
+      io.in(+gameId).emit('PLAY_GAME', ListOfGames.games[gameId]);
+    });
 
-      for (const [key, value] of rooms.entries()) {
-        if (!value.has(key)) {
-          availableRooms.push(key);
-        }
+    socket.on('HIT', (username) => {
+      const gameId = ListOfGames.getGameIdForPlayer(username);
+      const game = ListOfGames.games[gameId];
+      game.players[username].addCard(game.deck.draw());
+      io.emit('UPDATE_GAME', game); // Emit updated game state
+    });
+
+    socket.on('STAND', (username) => {
+      const gameId = ListOfGames.getGameIdForPlayer(username);
+      const game = ListOfGames.games[gameId];
+      // Logic for when a user stands...
+      io.emit('UPDATE_GAME', game);
+    });
+
+    socket.on('BET', (data) => {
+      const { username, amount } = data;
+      const gameId = ListOfGames.getGameIdForPlayer(username);
+      const game = ListOfGames.games[gameId];
+      game.players[username].placeBet(amount);
+      io.emit('UPDATE_GAME', game);
+    });
+
+    socket.on('LEAVE', (username) => {
+      const gameId = ListOfGames.getGameIdForPlayer(username);
+      const game = ListOfGames.games[gameId];
+      game.removePlayer(username);
+      io.emit('UPDATE_GAME', game);
+    });
+
+    socket.on('READY', (username) => {
+      const gameId = ListOfGames.getGameIdForPlayer(username);
+      const game = ListOfGames.games[gameId];
+      game.players[username].status = 'ready';
+      game.startGame();
+      io.emit('UPDATE_GAME', game);
+    });
+
+    socket.on('disconnect', () => {
+      const index = connectedSockets.indexOf(socket);
+      if (index !== -1) {
+        connectedSockets.splice(index, 1); // Remove the socket from the array when disconnected
       }
-
-      socket.emit('ROOMS', availableRooms);
     });
   });
 };
+function notifyUsers() {
+  for (let socket of connectedSockets) {
+    if (currentNames.names) {
+      socket.emit('USERS', [
+        Object.keys(currentNames.names),
+        ListOfGames.getAllGames(),
+      ]);
+    }
+  }
+}
 
-module.exports = { startIo };
+module.exports = { startIo, notifyUsers };
